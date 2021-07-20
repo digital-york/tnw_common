fde922bf369f9e00f932928759d4a95f57c3068b
Frank Feng <frank.feng@york.ac.uk>
2021-07-20 11:43:50 +0100

    add entry type/language fields

--- lib/tnw_common/shared/Constants.rb
+++ lib/tnw_common/shared/Constants.rb
@@ -1,30 +1,32 @@
 module TnwCommon
     module Shared
         class Constants
-            # FACET_ID => FACET Label
-            FACETS = {
-                FACET_REGISTER_OR_DEPARTMENT => "Register / Department",
-                FACET_DATE => "Date",
-                FACET_LANGUAGE => "Language",
-                FACET_PERSON_SAME_AS => "Person or Group"
-            FACET_PLACE_SAME_AS => "Place",
-                FACET_SECTION_TYPE => "Section Type",
-                FACET_SUBJECT => "Subject",
-            }.freeze
-
             # facets in format:
             # FACET_ID = SOLR_FIELD
             FACET_DATE = "date_facet_ssim".freeze
-            FACET_LANGUAGE = "language_facet_ssim".freeze  ## This facet isn't used in UI
+            FACET_ENTRY_TYPE = "entry_type_facet_ssim".freeze ## This facet isn't used in the UI
+            FACET_LANGUAGE = "language_facet_ssim".freeze  ## This facet isn't used in the UI
             FACET_PERSON_SAME_AS = "person_same_as_facet_ssim".freeze
             FACET_PLACE_SAME_AS = "place_same_as_facet_ssim".freeze
             FACET_REGISTER_OR_DEPARTMENT = "register_or_department_facet_ssim".freeze
             FACET_SECTION_TYPE = "section_type_facet_ssim".freeze
             FACET_SUBJECT = "subject_facet_ssim".freeze
 
+            # FACET_ID => FACET Label
+            FACETS = {
+                FACET_REGISTER_OR_DEPARTMENT => "Register / Department",
+                FACET_DATE => "Date",
+                FACET_LANGUAGE => "Language",
+                FACET_PERSON_SAME_AS => "Person or Group",
+                FACET_PLACE_SAME_AS => "Place",
+                FACET_SECTION_TYPE => "Section Type",
+                FACET_SUBJECT => "Subject"
+            }.freeze
+
             # Common Solr fields for both AR and TNA
             SOLR_FILED_COMMON_DATE_FULL_SSIM = "date_full_ssim".freeze
             SOLR_FIELD_COMMON_ENTRY_DATE_NOTE_TESIM = "entry_date_note_tesim".freeze
+            SOLR_FIELD_COMMON_ENTRY_TYPE_SEARCH = "entry_type_search".freeze
             SOLR_FILED_COMMON_LANGUAGE_SEARCH = "language_search".freeze
             SOLR_FILED_COMMON_NOTE_SEARCH = "note_search".freeze
             SOLR_FILED_COMMON_NOTE_TESIM = "note_tesim".freeze
